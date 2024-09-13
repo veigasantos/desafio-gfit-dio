0a5cee143e637b3e66901dfde34ce0b8d642a2e6
veigasantos <charles.sts@gmail.com>
2024-09-13 09:50:38 -0300

    Adiconado Comentários de comandos básicos GIT

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+/**
+----------------------------------------------------------------------------
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/veigasantos/desafio-gfit-dio.git
+git push -u origin main
+----------------------------------------------------------------------------
+
+
+----------------------------------------------------------------------------
+or push an existing repository from the command line
+git remote add origin https://github.com/veigasantos/desafio-gfit-dio.git
+git branch -M main
+git push -u origin main
+----------------------------------------------------------------------------
+*/
+
 console.log("Olá Git");

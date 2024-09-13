5b864f8c23b33ebc407e8afac509efc5eb6cca79
veigasantos <charles.sts@gmail.com>
2024-09-13 10:17:00 -0300

    adiocinada pasta src + arquivo js

--- index.js
+++ index.js
@@ -15,6 +15,15 @@ git remote add origin https://github.com/veigasantos/desafio-gfit-dio.git
 git branch -M main
 git push -u origin main
 ----------------------------------------------------------------------------
+
+
+----------------------------------------------------------------------------
+** Adcionando atualizações do projeto
+
+1 - git add .
+2 - git commit -m "Descrição da atualização"
+3 - git push -u origin main // envia para o GitHub
+
 */
 
 console.log("Olá Git");
